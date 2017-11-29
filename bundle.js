8ad7b3ae93c996b7884cee64e250c30d6f08fd04
Steven <codebusiness2014@gmail.com>
2017-11-29 16:34:33 -0500

    Temporary fix to background panning issue

--- bundle.js
+++ bundle.js
@@ -710,9 +710,9 @@ var Background = function () {
     this.render = this.render.bind(this);
 
     this.image = new Image();
-    this.image.src = 'images/test.png';
+    this.image.src = 'images/nebula.png';
 
-    this.speed = 3;
+    this.speed = 2;
     this.y = 0;
     this.draw = this.draw.bind(this);
     this.draw();
@@ -726,7 +726,7 @@ var Background = function () {
       this.ctx.drawImage(this.image, 0, this.y - this.canvas.height);
 
       if (this.y > this.canvas.height) {
-        this.image.src = 'images/stars_bottom.png';
+        this.image.src = 'images/nebula.png';
         this.y = 0;
       }
     }
